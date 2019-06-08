c2a4a54eeaaefe05e595d5757513f48d352ad7db
Aicha D <aicha1diallo@gmail.com>
2019-06-08 02:35:09 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,5 @@
 var index = require("../index.js")
-describe ('Name', ())
-it (returns "Susan" ', ()'
-expect(index.name).toEqual ('Susan')
+
 /*
 describe('what-is-a-test', () => {
   describe('Name', () => {
